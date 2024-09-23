cb92e66d2497eeb54a1d3a9be1cb05407db2d489
Donald Novasky <dnovasky@users.noreply.github.com>
2024-09-22 17:21:59 -0700

    Added an image to MainActivity

--- app/src/main/java/com/n0v4c0d3/simpleapp01/MainActivity.kt
+++ app/src/main/java/com/n0v4c0d3/simpleapp01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.n0v4c0d3.simpleapp01
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,8 +11,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var decreaseBtn: Button
     private lateinit var increaseBtn: Button
     private lateinit var counterText: TextView
+    private lateinit var personImageView: ImageView
+    private lateinit var toggleGlassesButton: Button
 
     private var currentCount = -99
+    private var glassesOn = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +24,11 @@ class MainActivity : AppCompatActivity() {
         decreaseBtn = findViewById(R.id.decrease_btn)
         increaseBtn = findViewById(R.id.increase_btn)
         counterText = findViewById(R.id.count_text)
+        personImageView = findViewById(R.id.person_image_view)
+        toggleGlassesButton = findViewById(R.id.toggle_glasses)
 
         currentCount = counterText.text.toString().toInt()
+        personImageView.setImageResource(R.drawable.no_glasses)
 
         decreaseBtn.setOnClickListener {
             currentCount -= 1
@@ -32,9 +39,22 @@ class MainActivity : AppCompatActivity() {
             currentCount += 1
             updateCountView()
         }
+
+        toggleGlassesButton.setOnClickListener {
+            glassesOn = !glassesOn
+            toggleGlasses()
+        }
     }
 
     private fun updateCountView() {
         counterText.text = currentCount.toString()
+    }
+
+    private fun toggleGlasses() {
+        val rId = when(glassesOn){
+            true -> R.drawable.glasses
+            false -> R.drawable.no_glasses
+        }
+        personImageView.setImageResource(rId)
     }
 }
